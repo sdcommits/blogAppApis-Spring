9464d84c8d8e019f32c8476536bc415e8537b4d9
Sheenam7 <sheenamdubey78@gmail.com>
2024-10-21 01:24:09 +0530

    entity and payload commit

--- src/main/java/com/codePro/blog/entities/User.java
+++ src/main/java/com/codePro/blog/entities/User.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Data
+//@Data
 //@Getter
 //@Setter
 //@NoArgsConstructor
@@ -36,8 +36,59 @@ public class User {
 	private String about;
 	
 	private String password;
-	
-	
-	
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(int id, String name, String email, String about, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.about = about;
+		this.password = password;
+	}
+
+	public User() {
+		super();
+	}
+	
+	
 }
--- src/main/java/com/codePro/blog/payloads/UserDto.java
+++ src/main/java/com/codePro/blog/payloads/UserDto.java
@@ -1,9 +1,13 @@
 package com.codePro.blog.payloads;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+//@Data
+//@Getter
+//@Setter
 @NoArgsConstructor
 public class UserDto {
 	
@@ -12,6 +16,47 @@ public class UserDto {
 	private String email;
 	private String about;
 	private String password;
+ 	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public UserDto(int id, String name, String email, String about, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.about = about;
+		this.password = password;
+	}
+	public UserDto() {
+		super();
+	}
 	
 	
 
